0596d3a5c021e53d883ecfc46afc05b76c8cb1d1
Rukai_develop <gt5pnanasi@gmail.com>
2025-08-03 22:38:33 +0900

    Update fastapi.py

--- hackathon_dir/fastapi.py
+++ hackathon_dir/fastapi.py
@@ -1,1 +1,11 @@
-aaa
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello from FastAPI!"}
+
+@app.get("/testing")
+async def read_root():
+    return {"message": "testing code..."}
