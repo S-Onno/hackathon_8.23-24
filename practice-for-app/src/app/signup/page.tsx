ba50efd7419ab2a80f0907f25ea34f17a3a513f6
noo-na <nathaliebanogon@gmail.com>
2025-08-12 18:19:53 +0900

    トップページ進捗

--- practice-for-app/src/app/signup/page.tsx
+++ practice-for-app/src/app/signup/page.tsx
@@ -1,9 +1,26 @@
-export default function LoginPage() {
+import Link from "next/link";
+
+export default function SighnUpPage() {
   return (
-    <main>
-    <div>
-      
-    </div>
+    <main className="min-h-screen flex items-center justify-center bg-gradient-to-b from-black via-indigo-900 to-purple-900 text-white px-4">
+      <div className="max-w-md w-full bg-white text-black p-8 rounded shadow">
+        <h2 className="text-2xl font-bold mb-6 text-center">新規登録</h2>
+        <form className="space-y-4">
+          <input type="text" placeholder="名前" className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-indigo-500"></input>
+          <input type="email" placeholder="メールアドレス" className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-indigo-500"></input>        
+          <input type="password" placeholder="パスワード" className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-indigo-500"></input>
+          <input type="password" placeholder="パスワード(確認用)" className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-indigo-500"></input>
+          <button className="w-full bg-indigo-600 text-white py-2 rounded hover:bg-indigo-700 transition">登録する</button>
+        </form>
+        <p className="mt-4 text-sm text-center">
+          すでにアカウントをお持ちですか？{" "}
+          <Link href="/login">
+          <button className="px-6 py-2 bg-blue-600 rounded hover:bg-blue-700 transition">
+            ログイン
+          </button>
+          </Link>
+        </p>
+      </div>
     </main>
   );
 }
