ff897aee27cf7d46713b31e141637d034bf31f84
yasutaka-uezu-k23c-gif <yasutaka-uezu-k23c@stu.kbc.ac.jp>
2025-08-13 18:08:00 +0900

    headerの追加

---
+++ practice-for-app/src/app/loading.tsx
@@ -0,0 +1,15 @@
+//ローディング
+
+export default function loading() {
+  return (
+    <div>
+      Loading...
+    </div>
+  )
+}
+
+//3秒待機
+// async function fetchArticles() {
+//   await new Promise((resolve)=> setTimeout(resolve, 3000))
+//   return articles
+// }
